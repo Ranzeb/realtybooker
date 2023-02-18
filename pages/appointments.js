1d64d9ad3c15c3f82dfe0b1ab5d257746f5e474d
Gabriele Ranzieri <104820500+Ranzeb@users.noreply.github.com>
2023-02-18 22:29:11 +0100

    Update appointments.js

--- pages/appointments.js
+++ pages/appointments.js
@@ -2,9 +2,11 @@ import AuthCheck from "@/components/AuthCheck";
 import Navbar from "@/components/Navbar";
 import SubNavbar from "@/components/SubNavbar";
 import {
-    VStack, Box, StackDivider, Heading, Text, Container
+    VStack, Box, StackDivider, Heading, Text, Container, HStack, Button
 } from '@chakra-ui/react';
 import { useState } from "react";
+import { TriangleDownIcon } from '@chakra-ui/icons';
+
 export default function Appointments() {
 
     const [appointments, setAppointments] = useState([
@@ -12,11 +14,15 @@ export default function Appointments() {
             date: "today",
             list: [
                 {
-                    title: "app1",
+                    email: "r.ranzieri@gmail.com",
+                    time: "09.00 - 12.00",
+                    user: "Gabriele Ranzieri",
                     desc: "The future can be even brighter but a goal without a plan is just a wish"
                 },
                 {
-                    title: "app2",
+                    email: "r.ranzieri@gmail.com",
+                    time: "09.00 - 12.00",
+                    user: "Gabriele Ranzieri",
                     desc: "The future can be even brighter but a goal without a plan is just a wish"
                 }
             ]
@@ -25,24 +31,61 @@ export default function Appointments() {
             date: "tomorrow",
             list: [
                 {
-                    title: "app1",
+                    email: "r.ranzieri@gmail.com",
+                    time: "09.00 - 12.00",
+                    user: "Gabriele Ranzieri",
                     desc: "The future can be even brighter but a goal without a plan is just a wish"
                 },
                 {
-                    title: "app2",
+                    email: "r.ranzieri@gmail.com",
+                    time: "09.00 - 12.00",
+                    user: "Gabriele Ranzieri",
                     desc: "The future can be even brighter but a goal without a plan is just a wish"
                 }
             ]
         },
     ]);
-            
-
-    function Feature({ title, desc, ...rest }) {
+    
+    function AdditionalInfo({location, timezone, email}) {
         return (
-          <Box p={5} shadow='md' borderWidth='1px' {...rest} mb={5}>
-            <Heading fontSize='xl'>{title}</Heading>
-            <Text mt={4}>{desc}</Text>
-          </Box>
+            <>
+                <HStack p={5} shadow='md' borderWidth='1px' borderRadius={10} mt={-5} mb={5} display={'grid'}>
+                    <Box mr={50}>
+                        <Heading fontSize='l'>Email: {email}</Heading>
+                        <Text mt={1}>Location: {location}</Text>
+                        <Text mt={1}>Timezone: {timezone}</Text>
+                    </Box>
+                    <Button mt={'20px'} fontSize='revert' colorScheme='red'>Cancel Appointment</Button>
+                </HStack>
+            </>
+        )
+    }
+
+    function Feature({ email, desc, time, user, ...rest }) {
+        const [isOpen, setIsOpen] = useState(false);
+
+        return (
+            <>
+                
+                <HStack p={5} shadow='md' borderWidth='1px' borderRadius={10} {...rest} mb={5}>
+                    <Box mr={50}>
+                        {time}
+                    </Box>
+                    <Box mr={50}>
+                        <Heading fontSize='l'>{user}</Heading>
+                        <Text mt={1}>{email}</Text>
+                    </Box>
+                    <Box>
+                        <TriangleDownIcon ml={50} onClick={() => {setIsOpen(currentIsOpen => !currentIsOpen)}}/>
+                    </Box>
+                </HStack>
+                {isOpen && <AdditionalInfo 
+                            email={email}
+                            timezone="UTC"
+                            location="Parma"
+                            />
+                }
+            </>
         )
       }
 
@@ -67,7 +110,9 @@ export default function Appointments() {
                                     return(
                                         <>
                                             <Feature
-                                                title={appointment.title}
+                                                email={appointment.email}
+                                                user={appointment.user}
+                                                time={appointment.time}
                                                 desc={appointment.desc}
                                             />
                                         </>
