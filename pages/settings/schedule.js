1b1e0c57a214f5e02a5bc9711991d9e6993f40fd
Ranzeb <r.ranzieri@gmail.com>
2023-03-23 22:51:33 +0100

    Update schedule.js

--- pages/settings/schedule.js
+++ pages/settings/schedule.js
@@ -2,6 +2,8 @@ import AuthCheck from "@/components/AuthCheck";
 import Navbar from "@/components/Navbar";
 import Sidebar from "@/components/Sidebar";
 import SubNavbar from "@/components/SubNavbar";
+import { firestore } from "@/lib/firebase";
+import { doc, setDoc, getDoc, addDoc, collection, query, where, getDocs } from "firebase/firestore";
 import {
     Button,
     Flex,
@@ -18,6 +20,7 @@ import {
     Divider
 } from '@chakra-ui/react';
 import { useState, useEffect } from "react";
+import { toast } from "react-hot-toast";
 
 const workingHours = {
     day: "",
@@ -41,7 +44,7 @@ export default function Schedule() {
     const changeSchedule = (value, day, type) => {
         const newSchedule = [...schedule];
         const currentWorkingHour = schedule.findIndex(obj => obj.day === day);
-        
+
         const newWorkingHour = {
             day: day,
             startingHour: type === "beginningHour" ? value : newSchedule[currentWorkingHour].startingHour,
@@ -58,6 +61,20 @@ export default function Schedule() {
 
     const handleSubmit = () => {
         //save state on DB then reload the page.
+
+        schedule.map(async (day) => {
+            try {
+                const docRef = await setDoc(doc(firestore, "availabilities", uid, day.day, "0"), {
+                    available: day.available,
+                    startingHour: day.startingHour,
+                    endingHour: day.endingHour,
+                })
+
+            } catch (e) {
+                console.log(e)
+            }
+        })
+        toast.success("Availabilities set successfully");
     }
 
     return (
@@ -82,7 +99,7 @@ export default function Schedule() {
                         <Heading lineHeight={1.1} fontSize={{ base: '2xl', md: '3xl' }} mb={5}>
                             Set your weekly schedule
                         </Heading>
-                        <form onSubmit={handleSubmit} method="post">
+
                         <FormControl id="email" display={'flex'} mt={5} mb={5} isRequired>
                             <SimpleGrid columns={[2, null, 1]} spacing='40px'>
                                 {days.map((day, id) => {
@@ -94,7 +111,7 @@ export default function Schedule() {
                                                 <Box display={'flex'} alignItems={'center'}>
                                                     <Input id={day + "BeginningHour"} mr={2} htmlSize={4} width='auto' onChange={e => changeSchedule(e.target.value, day, "beginningHour")} />
                                                     -
-                                                    <Input id={day + "EndingHour"} ml={2} htmlSize={4} width='auto' onChange={e => changeSchedule(e.target.value, day, "endingHour")}/>
+                                                    <Input id={day + "EndingHour"} ml={2} htmlSize={4} width='auto' onChange={e => changeSchedule(e.target.value, day, "endingHour")} />
                                                 </Box>
                                             </Box>
                                             <Divider />
@@ -110,11 +127,12 @@ export default function Schedule() {
                                 _hover={{
                                     bg: 'blue.500',
                                 }}
-                                type={"submit"}>
+                                type={"submit"}
+                                onClick={handleSubmit}>
                                 Submit
                             </Button>
                         </Stack>
-                        </form>
+
                     </Stack>
                 </Flex>
             </AuthCheck>
